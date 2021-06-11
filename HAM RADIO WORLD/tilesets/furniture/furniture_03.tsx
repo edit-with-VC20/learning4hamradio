57dd6e5b9d97d1338db8e666b7a3f1bff57b9e6b
Wolfgang Bachschwell <wolfgang@wbsfilm.at>
2021-06-11 12:18:17 +0200

    Copy over new booth

---
+++ HAM RADIO WORLD/tilesets/furniture/furniture_03.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="furniture_02" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="d9892fb-9b3ee140-a1ae-4674-9128-e15bead163dc.png" width="512" height="512"/>
+</tileset>
---
+++ HAM RADIO WORLD/tilesets/hamradiostuff/Fahnen_Aussenbereich.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Fahnen_Aussenbereich" tilewidth="32" tileheight="32" tilecount="150" columns="10">
+ <image source="Fahnen_Aussenbereich.png" width="320" height="480"/>
+</tileset>
