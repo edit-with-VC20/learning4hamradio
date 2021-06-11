62d87d7eeb773e1d8f330c731e5fe2fefe07ec0e
Wolfgang Bachschwell <wolfgang@wbsfilm.at>
2021-06-11 12:31:04 +0200

    add wbs link

--- HAM RADIO WORLD/tilesets/furniture/furniture_02.tsx
+++ HAM RADIO WORLD/tilesets/furniture/furniture_02.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="furniture_02" tilewidth="32" tileheight="32" tilecount="121" columns="11">
+<tileset version="1.5" tiledversion="1.7.0" name="furniture_02" tilewidth="32" tileheight="32" tilecount="121" columns="11">
  <image source="tileset1.png" width="352" height="352"/>
+ <tile id="56">
+  <properties>
+   <property name="colides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="colides" type="bool" value="false"/>
+  </properties>
+ </tile>
 </tileset>
